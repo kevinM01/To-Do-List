c8173a5a422a558f49ada362b7f7ef275b00d027
Kevin <kevinmasalia@gmail.com>
2021-07-04 17:57:10 +0530

    Add User Authentication

--- Taskmanager/models.py
+++ Taskmanager/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.db.models.base import Model
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 # Create your models here.
 class Task(models.Model):
-    name=models.CharField(max_length=200)
-    description=models.CharField(max_length=200)
-    is_completed=models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.name)
+
+
+@receiver(post_save,sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender,instance=None,created=False,**kwargs):
+    if created:
+        Token.objects.create(user=instance)
